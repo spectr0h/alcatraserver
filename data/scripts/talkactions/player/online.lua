2e1dc6c733edf9e6f21e57a3cd11587f3d252c21
spectr0h <willhubner@yahoo.com.br>
2023-08-27 20:37:52 -0300

    Gamestore sanguinde and Online fix

--- data/scripts/talkactions/player/online.lua
+++ data/scripts/talkactions/player/online.lua
@@ -2,29 +2,33 @@ local maxPlayersPerMessage = 10
 local playersOnline = TalkAction("!online")
 
 function playersOnline.onSay(player, words, param)
-	local hasAccess = player:getGroup():getAccess()
-	local players = Game.getPlayers()
-	local onlineList = {}
+    local hasAccess = player:getGroup():getAccess()
+    local players = Game.getPlayers()
+    local onlineList = {}
 
-	for _, targetPlayer in ipairs(players) do
-		if hasAccess or not targetPlayer:isInGhostMode() then
-			table.insert(onlineList, ("%s [%d]"):format(targetPlayer:getName(), targetPlayer:getLevel()))
-		end
-	end
+    for _, targetPlayer in ipairs(players) do
+        if hasAccess or not targetPlayer:isInGhostMode() then
+            table.insert(onlineList, ("%s [%d]"):format(targetPlayer:getName(), targetPlayer:getLevel()))
+        end
+    end
 
-	local playersOnlineList = #onlineList
-	player:sendTextMessage(MESSAGE_ATTENTION, ("%d players online!"):format(playersOnlineList))
+    local playersOnlineList = #onlineList
+    player:sendTextMessage(MESSAGE_ATTENTION, ("%d players online!"):format(playersOnlineList))
 
-	local listPlayers
-	for i = 1, playersOnlineList, maxPlayersPerMessage do
-		local j = math.min(i + maxPlayersPerMessage - 1, playersOnlineList)
-		local msg = table.concat(onlineList, ", ", i, j) .. "."
-		listPlayers = table.concat(onlineList, "\n", i, j)
-		player:sendTextMessage(MESSAGE_ATTENTION, msg)
-	end
+    local listPlayers = {}
+    for i = 1, playersOnlineList, maxPlayersPerMessage do
+        local j = math.min(i + maxPlayersPerMessage - 1, playersOnlineList)
+        local sublist = {}
+        for k = i, j do
+            table.insert(sublist, onlineList[k])
+        end
+        local msg = table.concat(sublist, ", ") .. "."
+        table.insert(listPlayers, table.concat(sublist, "\n"))
+        player:sendTextMessage(MESSAGE_ATTENTION, msg)
+    end
 
-	player:popupFYI("~ Players Online ~\n\n" .. listPlayers)
-	return true
+    player:popupFYI("~ Players Online ~\n\n" .. table.concat(listPlayers, "\n"))
+    return false
 end
 
 playersOnline:groupType("normal")
