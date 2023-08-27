aa7c16ed0f5301c88a3696af7dcbab77c302f8e8
spectr0h <willhubner@yahoo.com.br>
2023-08-27 20:21:40 -0300

    improve: lua format from EmmyLuaCodeStyle (#1513)
    Credits: @elsongabriel
    
    ---------
    
    Co-authored-by: Elson Gabriel <elsongabriel@hotmail.com>

--- data-otservbr-global/scripts/actions/other/gems.lua
+++ data-otservbr-global/scripts/actions/other/gems.lua
@@ -2,10 +2,43 @@ local lionsRockSanctuaryPos = Position(33073, 32300, 9)
 local lionsRockSanctuaryRockId = 1852
 local lionsRockSanctuaryFountainId = 6389
 
+<<<<<<< HEAD
+=======
+local shrine = {
+	-- ice shrine
+	[3029] = {
+		targetAction = 15001,
+		-- shrinePosition = {x = 32194, y = 31418, z = 2}, -- read-only
+		destination = { x = 33430, y = 32278, z = 7 },
+		effect = CONST_ME_ICEATTACK
+	},
+	-- fire shrine
+	[3030] = {
+		targetAction = 15002,
+		-- shrinePosition = {x = 32910, y = 32338, z = 15}, -- read-only
+		destination = { x = 33586, y = 32263, z = 7 },
+		effect = CONST_ME_MAGIC_RED
+	},
+	-- earth shrine
+	[3032] = {
+		targetAction = 15003,
+		-- shrinePosition = {x = 32973, y = 32225, z = 7}, -- read-only
+		destination = { x = 33539, y = 32209, z = 7 },
+		effect = CONST_ME_SMALLPLANTS
+	},
+	[3033] = {
+		targetAction = 15004,
+		-- shrinePosition = {x = 33060, y = 32713, z = 5}, -- read-only
+		destination = { x = 33527, y = 32301, z = 4 },
+		effect = CONST_ME_ENERGYHIT
+	}
+}
+
+>>>>>>> 03f065d6 (improve: lua format from EmmyLuaCodeStyle (#1513))
 local lionsRock = {
 	[25006] = {
 		itemId = 21442,
-		itemPos = {x = 33069, y = 32298, z = 9},
+		itemPos = { x = 33069, y = 32298, z = 9 },
 		storage = Storage.LionsRock.Questline,
 		value = 9,
 		item = 3030,
@@ -15,7 +48,7 @@ local lionsRock = {
 	},
 	[25007] = {
 		itemId = 21442,
-		itemPos = {x = 33069, y = 32302, z = 9},
+		itemPos = { x = 33069, y = 32302, z = 9 },
 		storage = Storage.LionsRock.Questline,
 		value = 9,
 		item = 3029,
@@ -25,7 +58,7 @@ local lionsRock = {
 	},
 	[25008] = {
 		itemId = 21440,
-		itemPos = {x = 33077, y = 32302, z = 9},
+		itemPos = { x = 33077, y = 32302, z = 9 },
 		storage = Storage.LionsRock.Questline,
 		value = 9,
 		item = 3033,
@@ -35,7 +68,7 @@ local lionsRock = {
 	},
 	[25009] = {
 		itemId = 21437,
-		itemPos = {x = 33077, y = 32298, z = 9},
+		itemPos = { x = 33077, y = 32298, z = 9 },
 		storage = Storage.LionsRock.Questline,
 		value = 9,
 		item = 9057,
@@ -50,15 +83,15 @@ function gems.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	-- Small emerald for Kilmaresh quest
 	-- see data\scripts\quests\kilmaresh\1-fafnars-wrath\7-four-masks.lua
 	if item.itemid == 3032 and target.uid == 40032
-				and player:getStorageValue(Storage.Kilmaresh.Sixth.Favor) >= 1
-				and not testFlag(player:getStorageValue(Storage.Kilmaresh.Sixth.FourMasks), 4) then
+			and player:getStorageValue(Storage.Kilmaresh.Sixth.Favor) >= 1
+			and not testFlag(player:getStorageValue(Storage.Kilmaresh.Sixth.FourMasks), 4) then
 		player:addItem(31371, 1) -- Ivory mask
 		item:remove(1)
-        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You hear a *click*. You can now lift the floor tile and discover a secret compartment. A mask made of ivory lies in it.")
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You hear a *click*. You can now lift the floor tile and discover a secret compartment. A mask made of ivory lies in it.")
 		player:setStorageValue(Storage.Kilmaresh.Sixth.Favor, player:getStorageValue(Storage.Kilmaresh.Sixth.Favor) + 1)
 		player:setStorageValue(Storage.Kilmaresh.Sixth.FourMasks, setFlag(player:getStorageValue(Storage.Kilmaresh.Sixth.FourMasks), 4))
 		return true
-	-- Enchanted helmet of the ancients
+		-- Enchanted helmet of the ancients
 	elseif player:getItemCount(3030) >= 1 and target.itemid == 3229 then
 		target:transform(3230)
 		target:decay()
@@ -68,6 +101,30 @@ function gems.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return true
 	end
 
+<<<<<<< HEAD
+=======
+	-- Gems teleport to feyrist
+	local ThreatenedDreams = Storage.Quest.U11_40.ThreatenedDreams
+	for index, value in pairs(shrine) do
+		if target.actionid == value.targetAction then
+			if player:getStorageValue(ThreatenedDreams.Mission01[1]) == 16 then
+				if item.itemid == index then
+					player:teleportTo(value.destination)
+					player:getPosition():sendMagicEffect(value.effect)
+					item:remove(1)
+					return true
+				else
+					player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+					return true
+				end
+			else
+				player:say("When the time comes, '" .. item:getName() .. "' will be accepted at this shrine.")
+				return true
+			end
+		end
+	end
+
+>>>>>>> 03f065d6 (improve: lua format from EmmyLuaCodeStyle (#1513))
 	-- Use gems in the tile of lions rock quest
 	local setting = lionsRock[target.uid]
 	if not setting then
--- data-otservbr-global/scripts/actions/quests/the_secret_library/fish.lua
+++ data-otservbr-global/scripts/actions/quests/the_secret_library/fish.lua
@@ -2,8 +2,13 @@ local fish = Action()
 
 function fish.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if player:getStorageValue(Storage.TheSecretLibrary.HighDry) == 2 then
+<<<<<<< HEAD
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE,"You have found a hawser.")
 		player:addItem(28707, 1)
+=======
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a hawser.")
+		player:addItem(32407, 1)
+>>>>>>> 03f065d6 (improve: lua format from EmmyLuaCodeStyle (#1513))
 		player:setStorageValue(Storage.TheSecretLibrary.HighDry, 3)
 		return true
 	end
